20235b8a0edb8f96a470ab4666e4586dbfb88505
Antoine <antoine.goustard@outlook.fr>
2021-09-24 12:55:38 +0200

    Commit project

---
+++ fichier.js
@@ -0,0 +1,62 @@
+// Etape 1 - Sélectionner nos éléments
+
+let input       = document.querySelector('#prix');
+let error       = document.querySelector('small');
+let formulaire  = document.querySelector('#formulaire');
+
+// Etape 2 - Cacher l'erreur
+
+error.style.display = "none";
+
+// Etape 3 - Générer un nombre aléatoire
+
+let nombreAleatoire = Math.floor(Math.random() * 1001);
+let coups = 0;
+let nombreChoisi;
+
+// Etape 4 - Créer la fonction vérifier
+
+function verifier(nombre) {
+  let instruction = document.createElement('div');
+  if (nombre < nombreAleatoire) {
+    instruction.textContent = "#"+coups+" ("+nombreChoisi+") C'est plus !";
+    instruction.className = 'instruction plus';
+  } else if (nombre > nombreAleatoire) {
+    instruction.textContent = "#"+coups+" ("+nombreChoisi+") C'est moins !";
+    instruction.className = 'instruction moins';
+  } else  {
+    instruction.textContent = "#"+coups+" ( " + nombreChoisi + " ) Félicitations !";
+    instruction.className = 'instruction fini';
+    input.disabled = true;
+  }
+  document.querySelector('#instructions').prepend(instruction);
+};
+
+
+// Etape 5 - Vérifier que l'utilisateur donne bien un nombre
+ 
+input.addEventListener('keyup', ()  => {
+    if (isNaN(input.value)) {
+      error.style.display = "inline";
+    } else {
+      error.style.display = "none";
+    }
+ });
+
+// Etape 6 - Agir à l'envoi du formulaire
+
+formulaire.addEventListener('submit',  (e) => {
+  e.preventDefault();
+
+  if (isNaN(input.value) || input.value == '') {
+    input.style.borderColor = 'red';
+  } else {
+    coups++;
+    input.style.borderColor = 'silver';
+    nombreChoisi = input.value;
+    input.value = '';
+    verifier(nombreChoisi);
+  }
+});
+
+
